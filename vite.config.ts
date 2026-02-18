6faa1181244f9e74edd7c5d1d4e3a04acaabb06a
EarthSocial <openclaw@earthsocial.app>
2026-02-18 10:09:44 +0000

    Deploy with /earthsocial/ base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/earthsocial/',
   server: {
     host: true,
     port: 5173
